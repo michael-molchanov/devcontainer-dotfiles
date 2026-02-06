746894cde0557af3a9e8365d1871e0f101a9e2e3
Mykhailo Molchanov <mykhailo.molchanov@intellias.com>
2026-02-06 02:26:43 +0100

    Fix unminimize.
    Signed-off-by: Mykhailo Molchanov <mykhailo.molchanov@intellias.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xeu
 
-sudo unminimize
+yes | sudo unminimize
 sudo apt-get install man-db
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
