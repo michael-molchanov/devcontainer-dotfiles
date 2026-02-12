554fb7b27242c1b1c8cb4c5cffdf8fc5c7736b9e
Mykhailo Molchanov <mykhailo.molchanov@intellias.com>
2026-02-12 01:59:20 +0100

    Cleanup.
    Signed-off-by: Mykhailo Molchanov <mykhailo.molchanov@intellias.com>

--- install.sh
+++ install.sh
@@ -25,18 +25,13 @@ brew install \
   fzf \
   gitlab-ci-local \
   go \
-  graphviz \
   helm \
   helmfile \
-  imagemagick \
   jq \
   k9s \
   kubectx \
   kubernetes-cli \
-  lua-language-server \
-  luarocks \
   man-db \
-  mermaid-cli \
   node \
   numpy \
   openjdk \
@@ -73,6 +68,3 @@ git config --global gpg.ssh.allowedsignersfile "~/.ssh/allowed_signers"
 mkdir -p "${HOME}/.ssh"
 chmod 700 "${HOME}/.ssh"
 echo "${GIT_USER_EMAIL} namespaces=\"git\" ${PUBLIC_SSH_KEY}" >~/.ssh/allowed_signers
-
-cp -fR nvim "${XDG_CONFIG_HOME:-$HOME/.config}"/
-cp -fR lazygit "${XDG_CONFIG_HOME:-$HOME/.config}"/
